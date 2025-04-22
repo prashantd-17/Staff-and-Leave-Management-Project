79cf17df2e5cfbe07b659dcc899d8e702ccfc6eb
prashant deshpande <prashantdeshpande2302@gmail.com>
2025-04-22 12:31:01 +0530

    fronend env fix

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: 'https://your-production-api.com',
+    apiUrl: 'https://staff-and-leave-management-project.onrender.com',
   };
   
